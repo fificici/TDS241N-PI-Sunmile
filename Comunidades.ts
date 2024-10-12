f14cffbd5cec086db28601f2b2b894f15634ab05
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-10-11 21:22:26 -0300

    Update Comunidades.ts

--- Comunidades.ts
+++ Comunidades.ts
@@ -1,12 +1,16 @@
 import { Conta } from "./contas"
+import { Post } from "./posts"
+import { Rede } from "./rede"
 
 export class Comunidade{
+    posts_comunidades: Array<Post>
     id_Comunidade: number
     titulo: string
     descricao: string
     seguidores: Array<Conta>
 
-    constructor(IDComunidade: number, titulo_Comunidade: string, descricao_comunidade: string, seguidores_da_comunidade: Array<Conta>){
+    constructor(PostsComunidades: Array<Post>, IDComunidade: number, titulo_Comunidade: string, descricao_comunidade: string, seguidores_da_comunidade: Array<Conta>){
+        this.posts_comunidades = PostsComunidades
         this.id_Comunidade = IDComunidade
         this.titulo = titulo_Comunidade
         this.descricao = descricao_comunidade
@@ -27,13 +31,13 @@ export class Comunidade{
 }
 
 export class SubComunidade extends Comunidade {
-    Comunidade_Mae: Comunidade
+    
+    comunidade_Mae: Comunidade
     comunidade_Adulta: boolean
 
-    constructor(Id_da_comunidade: number, titulo_Comunidade: string, descricao_comunidade: string,comunidade_para_maiores_de_18: boolean, Classe_de_Origem: Comunidade, seguidores_da_comunidade: Array<Conta>){
-        super(Id_da_comunidade, titulo_Comunidade, descricao_comunidade, seguidores_da_comunidade)
-        this.Comunidade_Mae = Classe_de_Origem
+    constructor(PostsComunidades: Array<Post>, Id_da_comunidade: number, titulo_Comunidade: string, descricao_comunidade: string,comunidade_para_maiores_de_18: boolean, Classe_de_Origem: Comunidade, seguidores_da_comunidade: Array<Conta>){
+        super(PostsComunidades, Id_da_comunidade, titulo_Comunidade, descricao_comunidade, seguidores_da_comunidade)
+        this.comunidade_Mae = Classe_de_Origem
         this.comunidade_Adulta = comunidade_para_maiores_de_18
     }
 }
-
