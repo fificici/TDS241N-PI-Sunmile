b5a34f1615a56ab92b423710d597f82aacdd827e
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-10-11 21:25:22 -0300

    Update rede.ts

--- rede.ts
+++ rede.ts
@@ -1,10 +1,11 @@
 import { Conta } from "./contas";
-import { Comunidade } from "./comunidades";
+import { Comunidade, SubComunidade } from "./comunidades";
 import { Post } from "./posts";
 
 export class Rede{
     contas: Array<Conta>
     comunidades: Array<Comunidade>
+    sub_comunidades:Array<Comunidade>
     posts: Array<Post>
 
     constructor(BancoContas: Array<Conta>, BancoComunidades: Array<Comunidade>, BancoPosts: Array<Post>){
@@ -13,5 +14,38 @@ export class Rede{
         this.posts = BancoPosts
     }
 
-    
+    GetContas(): Array<Conta>{
+        return this.contas
+    }
+
+    public CriarConta(conta: Conta): void{
+        this.contas.push(conta)
+    }
+
+    public GetContaById(Id_Conta: number): Conta {
+        for (let conta of this.contas) {
+            if (conta.GetIdConta() === Id_Conta) {
+                return conta
+            }
+        }
+        throw new Error("Conta não encontrada")
+    }
+
+    GetContaByNomeUsuario(nomedePerfil: string): Conta {
+        for (let conta of this.contas) {
+            if (conta.GetNomeUsuario() === nomedePerfil){
+                return conta
+            }
+        }
+        throw new Error("Conta nâo encontrada")
+    }
+
+    ArmazenarPosts(post: Post): void{
+        for (const comunidade of this.comunidades) {
+            if(post.GetDestinatario() === comunidade.titulo){
+                comunidade.posts_comunidades.push(post)
+            }
+        }
+        throw new Error("Comunidade não encontrada")
+    }
 }
