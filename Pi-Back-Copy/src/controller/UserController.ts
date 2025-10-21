daeb2590bcbd227633d5f6bb415db3dbcd618e43
Felipe Cantini <114841536+fificici@users.noreply.github.com>
2025-10-20 21:37:51 -0300

    Update UserController.ts

--- Pi-Back-Copy/src/controller/UserController.ts
+++ Pi-Back-Copy/src/controller/UserController.ts
@@ -48,4 +48,104 @@ export class UserController {
           return res.status(500).json({ message: "Internal server error" })
         }
     }
+
+    async createStudent(req: Request, res: Response): Promise<Response> {
+
+        try {
+
+          const { name, username, email, cpf, birth_date, password, bio, institution, enrollment } = req.body
+
+          if (!name || !email || !password || !username || !cpf || !birth_date || !bio || !institution || !enrollment) {
+
+            return res
+              .status(400)
+              .json({ message: "Something is missing!" })
+          }
+    
+          const userExists = await userRepository.findByEmail(email)
+          const verificationBirthDate = await userRepository.verifyBirthDate(birth_date)
+          const verificationPassword = await userRepository.verifyPassword(password)
+
+          if (userExists) {
+            return res.status(409).json({ message: "Email already in use!" })
+          }
+
+          if (verificationBirthDate) {
+            return res.status(409).json({ message: "Birth date format error!" })
+          }
+
+          if (verificationPassword) {
+            return res.status(409).json({ message: "Insert a stronger password!" })
+          }
+    
+          const user = await userRepository.createAndSave({
+            name,
+            username,
+            email,
+            cpf,
+            birth_date,
+            password,
+            bio,
+            institution,
+            enrollment
+          })
+
+          return res.status(201).json(user)
+
+        } catch (error) {
+
+          console.error(error)
+          return res.status(500).json({ message: "Internal server error" })
+        }
+    }
+
+    async createProfessional(req: Request, res: Response): Promise<Response> {
+
+      try {
+
+        const { name, username, email, cpf, birth_date, password, bio, pro_registration } = req.body
+
+        if (!name || !email || !password || !username || !cpf || !birth_date || !bio || !pro_registration) {
+
+          return res
+            .status(400)
+            .json({ message: "Something is missing!" })
+        }
+  
+        const userExists = await userRepository.findByEmail(email)
+        const verificationBirthDate = await userRepository.verifyBirthDate(birth_date)
+        const verificationPassword = await userRepository.verifyPassword(password)
+
+        if (userExists) {
+          return res.status(409).json({ message: "Email already in use!" })
+        }
+
+        if (verificationBirthDate) {
+          return res.status(409).json({ message: "Birth date format error!" })
+        }
+
+        if (verificationPassword) {
+          return res.status(409).json({ message: "Insert a stronger password!" })
+        }
+  
+        const user = await userRepository.createAndSave({
+          name,
+          username,
+          email,
+          cpf,
+          birth_date,
+          password,
+          bio,
+          pro_registration
+        })
+
+        return res.status(201).json(user)
+
+        } catch (error) {
+
+        console.error(error)
+        return res.status(500).json({ message: "Internal server error" })
+        }
+    }
+
 }
