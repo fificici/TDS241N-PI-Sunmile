49b72b679e5b872df43c1d027b636694fc612949
Felipe Cantini <114841536+fificici@users.noreply.github.com>
2025-11-11 00:06:05 +0000

    Add files via upload

---
+++ Pi-Back - Copia/src/controller/UserController.ts
@@ -0,0 +1,137 @@
+import { Request, Response } from "express"
+import { UserRepository } from "../repositories/UserRepository"
+
+const userRepository = new UserRepository()
+
+export class UserController {
+
+    async createUser(req: Request, res: Response): Promise<Response> {
+
+        try {
+
+          const { name, username, email, cpf, birth_date, password } = req.body
+
+          if (!name || !email || !password || !username || !cpf || !birth_date) {
+
+            return res
+              .status(400)
+              .json({ message: "Something is missing!" })
+          }
+    
+          const userExists = await userRepository.findByEmail(email)
+          const verificationBirthDate = await userRepository.verifyBirthDate(birth_date)
+          const verificationPassword = await userRepository.verifyPassword(password)
+
+          if (userExists) {
+            return res.status(409).json({ message: "Email already in use!" })
+          }
+
+          if (verificationBirthDate) {
+            return res.status(409).json({ message: "Birth date format error!" })
+          }
+
+          if (verificationPassword) {
+            return res.status(409).json({ message: "Insert a stronger password!" })
+          }
+    
+          const user = await userRepository.createAndSave({
+            name,
+            username,
+            email,
+            cpf,
+            birth_date,
+            password
+          })
+
+          return res.status(201).json(user)
+
+        } catch (error) {
+
+          console.error(error)
+          return res.status(500).json({ message: "Internal server error" })
+        }
+    }
+
+    async updateUser(req: Request, res: Response): Promise<Response> {
+
+      try {
+
+        const id = Number(req.params.id) || req.user.id
+  
+        if (req.user.role !== "admin" && id !== req.user.id) {
+
+          return res.status(403).json({ message: "Access denied" })
+        }
+  
+        const user = await userRepository.findById(id)
+
+        if (!user) return res.status(404).json({ message: "User not found" })
+  
+        const { name, username, email, password } = req.body
+  
+        if (name) user.name = name
+        if (password) user.password = password
+
+        if (email) {
+
+          const emailExists = await userRepository.findByEmail(email)
+
+          if (emailExists && emailExists.id !== user.id) {
+
+            return res.status(409).json({ message: "Email already in use" })
+
+          }
+
+          user.email = email
+        }
+
+        if (username) {
+
+          const usernameExists = await userRepository.findByUsername(username)
+
+          if (usernameExists && usernameExists.id !== user.id) {
+
+            return res.status(409).json({ message: "Username already in use" })
+
+          }
+
+          user.username = username
+        }
+        
+  
+        const updatedUser = await userRepository.saveUser(user)
+
+        return res.json(updatedUser)
+
+      } catch (error) {
+
+        console.error(error)
+        return res.status(500).json({ message: "Internal server error" })
+      }
+    }
+
+    async deleteUser(req: Request, res: Response): Promise<Response> {
+
+      try {
+
+        const id = Number(req.params.id) || req.user.id
+  
+        if (req.user.role !== "admin" && id !== req.user.id) {
+          return res.status(403).json({ message: "Access denied" })
+        }
+  
+        const user = await userRepository.findById(id);
+        if (!user) return res.status(404).json({ message: "User not found" })
+  
+        await userRepository.removeUser(user)
+
+        return res.status(204).send()
+
+      } catch (error) {
+
+        console.error(error)
+
+        return res.status(500).json({ message: "Internal server error" })
+      }
+    }
+}
---
+++ Pi-Back - Copia/src/repositories/UserRepository.ts
@@ -0,0 +1,39 @@
+import { AppDataSource } from '../config/data-source'
+import { User } from '../models/User'
+import { Repository } from 'typeorm'
+
+export class UserRepository {
+
+  private repository: Repository<User>
+
+  constructor() {
+
+    this.repository = AppDataSource.getRepository(User)
+  }
+
+  async findById(id: number): Promise<User | null> {
+
+    return this.repository.findOneBy({ id })
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+
+    return this.repository.findOneBy({ email })
+  }
+
+  async createAndSave(data: Partial<User>): Promise<User> {
+
+    const user = this.repository.create(data)
+    return this.repository.save(user)
+  }
+
+  async saveUser(user: User): Promise<User> {
+
+    return this.repository.save(user)
+  }
+
+  async removeUser(user: User): Promise<User> {
+
+    return this.repository.remove(user)
+  }
+}
